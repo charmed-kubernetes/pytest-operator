71f6f42acddce8357a691835defbfebdb8a09997
Cory Johns <johnsca@gmail.com>
2021-02-16 16:23:15 -0500

    Add helpers to build library and render charms
    Charm libraries using this for integration tests need to be able to
    build the library and then inject it into their test charms.

--- tests/test_pytest_operator.py
+++ tests/test_pytest_operator.py
@@ -1,8 +1,26 @@
+import logging
+from pathlib import Path
+
 from pytest_operator import OperatorTest
+
+
+log = logging.getLogger(__name__)
 
 
 class PluginTest(OperatorTest):
     async def test_build_and_deploy(self):
+        lib_path = Path(__file__).parent.parent
+        pytest_operator = await self.build_lib(lib_path)
+        charms = self.render_charms(
+            "tests/data/charms/reactive-framework",
+            "tests/data/charms/operator-framework",
+            include=["requirements.txt"],
+            context={
+                "pytest_operator": pytest_operator,
+            },
+        )
+        req_path = charms[1] / "requirements.txt"
+        assert f"file://{pytest_operator}#egg=pytest_operator" in req_path.read_text()
         bundle = self.render_bundle(
             # Normally, this would just be a filename like for the charms, rather
             # than an in-line YAML dump, but for visibility purposes in using this
@@ -18,11 +36,9 @@ class PluginTest(OperatorTest):
                     charm: {{ charms["operator-framework"] }}
                     num_units: 1
             """,
-            charms=await self.build_charms(
-                "tests/data/charms/reactive-framework",
-                "tests/data/charms/operator-framework",
-            ),
+            charms=await self.build_charms(*charms),
         )
+        log.info("Deploying bundle")
         await self.model.deploy(bundle)
         await self.model.wait_for_idle()
         for unit in self.model.units.values():
