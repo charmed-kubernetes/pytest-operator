512dc66df0096826dd068a830b4c06a6c30b0889
Cory Johns <johnsca@gmail.com>
2021-08-26 09:15:58 -0400

    Bump version for release and update PyPI description

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
 from setuptools import setup, find_packages
 
 setup(
     author="Cory Johns",
     author_email="cory.johns@canonical.com",
     description="Fixtures for Operators",
-    long_description="Fixtures for Operators",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
     classifiers=[
         "Framework :: Pytest",
         "Programming Language :: Python",
@@ -22,7 +24,7 @@ setup(
     name="pytest-operator",
     packages=find_packages(include=["pytest_operator"]),
     url="https://github.com/charmed-kubernetes/pytest-operator",
-    version="0.8.1",
+    version="0.8.2",
     zip_safe=True,
     install_requires=[
         "ipdb",
