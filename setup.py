07efec0117e1fe6f3f97a056b60ae761eeda466b
Andrew Scribner <ca.scribner+1@gmail.com>
2021-09-07 10:59:47 -0400

    Bump version for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name="pytest-operator",
     packages=find_packages(include=["pytest_operator"]),
     url="https://github.com/charmed-kubernetes/pytest-operator",
-    version="0.8.2",
+    version="0.8.3",
     zip_safe=True,
     install_requires=[
         "ipdb",
