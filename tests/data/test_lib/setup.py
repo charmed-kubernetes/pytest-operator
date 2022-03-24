182f46d264c8e3ff94e74a0f863f94a4e269e254
Robert Gildein <robert.gildein@canonical.com>
2022-03-25 00:24:15 +0100

    Replace build pytest_operator with test_lib (#63)
    Use dumb test-lib instead of building pytest_operator as a library in
    integration test. This will reduce the time of integration tests.

---
+++ tests/data/test_lib/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name="pytest-operator-test-lib",
+      version="1.0.0",
+      description="Pytest-operator test library.",
+      author="tester",
+      packages=[],
+)
