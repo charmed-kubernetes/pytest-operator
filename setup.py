f8ab499f6c5d7b11fbaf0ffcd5af6529411720c0
Adam Dyess <adam.dyess@canonical.com>
2024-08-07 09:46:21 -0500

    Publishing 0.36.0

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     license="MIT license",
     include_package_data=True,
@@ -26,7 +27,7 @@ setup(
     packages=find_packages(include=["pytest_operator"]),
     package_data={'pytest_operator': ['py.typed']},
     url="https://github.com/charmed-kubernetes/pytest-operator",
-    version="0.35.0",
+    version="0.36.0",
     zip_safe=True,
     install_requires=[
         "ipdb",
